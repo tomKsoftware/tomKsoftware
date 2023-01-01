183666159e0163b45c28f0ca00484cb234120fbf
Tom Kristensen <tom.kristensen0372@gmail.com>
2023-01-01 17:12:01 +0100

    melding ifm commit

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
 import string
 
 print ("Hello world")
-
-
-
-print("du trodde kanskje det var alt.   nei nei.     god jul og godt nyttår!!!")
-
-
-
-
-print ("her er det tydelig at også Tomas har værrt inne i bildet.   tralala")
+print ("her er det tydelig at også Tomas har værrt inne i bildet.\n\n\n\n\t\t\t\t\t\ttralala")
