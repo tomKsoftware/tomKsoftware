682c04dbb744c5b3346b40e3d7c96dbd0294484e
Tomas Erling Kristensen <tomaserlingkristensen@gmail.com>
2023-01-01 16:08:03 +0100

    toms endringer tillagt tomas sine endringer

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
+import string
+
 print ("Hello world")
 
 
 
 print("du trodde kanskje det var alt.   nei nei.     god jul og godt nyttår!!!")
+
+
+
+
+print ("her er det tydelig at også Tomas har værrt inne i bildet.   tralala")
