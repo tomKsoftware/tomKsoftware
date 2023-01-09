4f51f9c7059ff63b81bba0e0af7551f65f9a70e5
Tom <tom@Olavs-Air.localdomain>
2023-01-09 23:39:00 +0100

    her er min commit melding

--- test.py
+++ test.py
@@ -3,3 +3,5 @@ import string
 print ("Hello world")
 print ("her er det tydelig at også Tomas har værrt inne i bildet.\n\n\n\n\t\t\t\t\t\ttralala")
 print(2**2)
+print("fra toms nye mac")
+print (2+2)
